a1d511082e42d82d851116af7b2138f51298fbca
okunoryo <okuno.ryo.411@gmail.com>
2014-05-22 00:27:03 +0900

    4th

--- shaberitakunaio.sh
+++ shaberitakunaio.sh
@@ -6,6 +6,12 @@ do
   echo "[入力受付中]\n"
   read INPUT
   say $INPUT;
-  echo  "（´・ω・｀）\n"
-  echo "-----------------------"
+
+  if test $(($RANDOM % 5)) -eq 0 ; then
+      echo "(｀・ω・´)\n"
+    else
+      echo "（´・ω・｀）\n"
+      fi 
+ 
+    echo "-----------------------"
 done
