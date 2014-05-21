9f0b7d661082980189e3a82f6e160477b4b93bd3
okunoryo <okuno.ryo.411@gmail.com>
2014-05-21 23:27:08 +0900

    1st
    first commit

---
+++ readme.txt
@@ -0,0 +1,4 @@
+【操作方法】
+1.システム設定＞音声入力と読み上げ＞システムの声＞Kyoko
+2.ターミナルを開き[ chmod +x shaberitakunaio.sh ]
+3.ターミナルで[./shaberitakunaio.sh]
---
+++ shaberitakunaio.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+echo "しゃべりたくないお(´・ω・｀)\n"
+say しゃべりたくないお
+while true
+do
+  echo "[入力受付中]\n"
+  read INPUT
+  say $INPUT;
+  echo  "（´・ω・｀）\n"
+  echo "-----------------------"
+done
